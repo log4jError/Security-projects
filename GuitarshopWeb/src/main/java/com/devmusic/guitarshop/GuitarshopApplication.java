a76822dd6a1da529dfca1ac6369b28bc53faf939
log4jError <mahajanvishalb@gmail.com>
2018-08-23 22:27:50 +0530

    initial coomit

---
+++ GuitarshopWeb/src/main/java/com/devmusic/guitarshop/GuitarshopApplication.java
@@ -0,0 +1,12 @@
+package com.devmusic.guitarshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuitarshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuitarshopApplication.class, args);
+	}
+}
